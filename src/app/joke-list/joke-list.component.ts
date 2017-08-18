f1c92fd0b3372e8facd1236f7d5bd40d8539614a
dthuesen <dthuesen@gmx.de>
2017-08-18 15:02:27 +0200

    Implement lifecycle hooks and add random jokes

--- src/app/joke-list/joke-list.component.ts
+++ src/app/joke-list/joke-list.component.ts
@@ -11,21 +11,31 @@ import { JokeFormComponent } from '../joke-form/joke-form.component'
 export class JokeListComponent {
 
   jokes: Joke[] = [];
+  jokesStock: Joke[];
 
-  // constructor() {
-  //   this.jokes = [
-  //     new Joke('What did the cheese say when it looked in the mirror?', 'Hello-me (Halloumi)'),
-  //     new Joke('What kind of cheese do you use to disguise a small horse?', 'Mask-a-pony (Mascarpone)'),
-  //     // tslint:disable-next-line:quotemark
-  //     new Joke('A kid thew a lump of cheddar at me', "I thought 'That's not very mature'"),
-  //   ]
-  // }
+  constructor() {
+    this.jokesStock = [
+      new Joke('What did the cheese say when it looked in the mirror?', 'Hello-me (Halloumi)'),
+      new Joke('What kind of cheese do you use to disguise a small horse?', 'Mask-a-pony (Mascarpone)'),
+      new Joke('What hangs on the wall and gives everyone a hand?', 'A towl'),
+      new Joke('What hangs on the wall and if the Kukuksuhr falls down it breaks?', 'A very bad joke'),
+      // tslint:disable-next-line:quotemark
+      new Joke('A kid thew a lump of cheddar at me', "I thought 'That's not very mature'"),
+    ]
+  }
+
+  randomNumber() {
+    return Math.floor(Math.random() * 5);
+  }
 
   // addJoke(joke) {
   //   this.jokes.unshift(joke);
   // }
+  // addJoke() {
+  //   this.jokes.unshift(new Joke('What did the cheese say when it looked in the mirror?', 'Hello-me (Halloumi)'));
+  // }
   addJoke() {
-    this.jokes.unshift(new Joke('What did the cheese say when it looked in the mirror?', 'Hello-me (Halloumi)'));
+    this.jokes.unshift(this.jokesStock[this.randomNumber()]);
   }
 
   deleteJoke(joke) {
